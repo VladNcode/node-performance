8ed4aaadb9e15de6d869966ddaebd0ef773e528c
vladncode <vladncode@gmail.com>
2022-03-11 11:00:58 +0200

    switched to pm2

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// const cluster = require('cluster');
+// const os = require('os');
 const express = require('express');
 const app = express();
 
@@ -13,14 +15,32 @@ app.get('/', (req, res) => {
   // JSON.parse("{}") => {}
   // [5,1,3,4,2].sort()
 
-  res.send('Performance example');
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(3000, (req, res) => {
-  console.log('Server listening on port 3000');
-});
+// console.log('Running server.js');
+// if (cluster.isMaster) {
+//   console.log('Master process has been started!');
+
+//   const numberOfWorkers = os.cpus().length;
+//   console.log(numberOfWorkers);
+
+//   for (let i = 0; i < numberOfWorkers; i++) {
+//     cluster.fork();
+//   }
+// } else {
+//   console.log('Worker process has been started!');
+
+//   app.listen(3000, (req, res) => {
+//     // console.log('Server listening on port 3000');
+//   });
+// }
+
+console.log('Running server.js');
+console.log('Worker process has been started!');
+app.listen(3000);
